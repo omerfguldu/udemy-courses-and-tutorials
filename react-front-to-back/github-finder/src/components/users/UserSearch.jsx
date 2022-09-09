0a377de524b7aea404648ec75b8dcf4d511c9b8d
gulduomer <43722346+omerfguldu@users.noreply.github.com>
2022-09-09 19:15:04 +0300

    input bos girildiginde alert ile uyari mesaji gosterildi. secilen kullaniciya ait ayrintili hesap bilgilerinin yer aldigi user componenti tamamlandi. context icinde yer alan fonksiyonlar ayri bir dosyaya alindi.

--- react-front-to-back/github-finder/src/components/users/UserSearch.jsx
+++ react-front-to-back/github-finder/src/components/users/UserSearch.jsx
@@ -1,27 +1,28 @@
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
+import { searchUsers } from "../../context/github/GithubActions";
 
 function UserSearch() {
-  const { users, clearUsers, searchUsers } = useContext(GithubContext);
+  const { users, dispatch } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
   const [text, setText] = useState("");
 
   const handleChange = (e) => {
     setText(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (text === "") {
-      alert("Please enter something!");
+      setAlert("Please enter something!", "error");
     } else {
-      searchUsers(text);
+      dispatch({ type: "SET_LOADING" });
+      const users = await searchUsers(text);
+      dispatch({ type: "GET_USERS", payload: users });
       setText("");
     }
-  };
-
-  const handleClear = () => {
-    clearUsers();
   };
 
   return (
@@ -50,7 +51,10 @@ function UserSearch() {
       <div>
         {users.length > 0 && (
           <div>
-            <button onClick={handleClear} className="btn btn-ghost btn-lg">
+            <button
+              onClick={() => dispatch({ type: "CLEAR_USERS" })}
+              className="btn btn-ghost btn-lg"
+            >
               Clear
             </button>
           </div>
