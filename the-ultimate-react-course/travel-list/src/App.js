687c72a0aba437b882e78a5394b983cb873803fb
omerfguldu <omerfguldu@gmail.com>
2024-02-10 17:59:53 +0300

    travel list project started, add item and remove item logic added

---
+++ the-ultimate-react-course/travel-list/src/App.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  const [items, setItems] = useState(initialItems);
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form items={items} updateItems={setItems} />
+      <PackingList items={items} updateItems={setItems} />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form({ items, updateItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    updateItems([...items, newItem]);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ items, updateItems }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            items={items}
+            updateItems={updateItems}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item, items, updateItems }) {
+  function handleRemove(id) {
+    updateItems(items.filter((item) => item.id !== id));
+  }
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => handleRemove(item.id)}>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list and you already packed X (X%)</em>
+    </footer>
+  );
+}
