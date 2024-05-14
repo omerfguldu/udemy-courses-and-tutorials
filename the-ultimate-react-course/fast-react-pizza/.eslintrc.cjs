c932196e0625bbd294cb6a41240318179bfcfc3a
omerfguldu <omerfguldu@gmail.com>
2024-05-14 18:33:26 +0300

    react router with data loading

--- the-ultimate-react-course/fast-react-pizza/.eslintrc.cjs
+++ the-ultimate-react-course/fast-react-pizza/.eslintrc.cjs
@@ -18,5 +18,6 @@ module.exports = {
     ],
     "react/prop-types": "off",
     "no-unused-vars": "warn",
+    "react/no-unescaped-entities": "off",
   },
 };
--- the-ultimate-react-course/fast-react-pizza/src/ui/Header.jsx
+++ the-ultimate-react-course/fast-react-pizza/src/ui/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>Sukru</p>
+      <Username />
     </header>
   );
 }
