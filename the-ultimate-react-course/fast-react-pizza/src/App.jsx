3ea872a44966032dd235dbfbdcbc42f38e2d18e8
omerfguldu <omerfguldu@gmail.com>
2024-05-25 21:05:17 +0300

    fast react pizza app completed

--- the-ultimate-react-course/fast-react-pizza/src/App.jsx
+++ the-ultimate-react-course/fast-react-pizza/src/App.jsx
@@ -2,12 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./ui/Home";
 import Error from "./ui/Error";
-import Menu, { loader, loader as menuLoader } from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -39,6 +40,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
